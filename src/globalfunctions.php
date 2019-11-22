d4b7907293dc30950483c11f98d945ce320c2ee5
Gabriel Mourão <gabriel.mourao@blueticket.com.br>
2019-11-21 22:49:50 -0300

    criada função ddd

---
+++ src/globalfunctions.php
@@ -0,0 +1,13 @@
+<?php
+
+include __DIR__ . '/../vendor/autoload.php';
+
+use Symfony\Component\VarDumper\Dumper\HtmlDumper;
+use Symfony\Component\VarDumper\Cloner\VarCloner;
+use Symfony\Component\VarDumper\Dumper\CliDumper;
+
+function ddd($variable)
+{
+    ('cli' === PHP_SAPI ? (new CliDumper()) : new HtmlDumper())->dump((new VarCloner())->cloneVar($variable));
+    die(1);
+}
